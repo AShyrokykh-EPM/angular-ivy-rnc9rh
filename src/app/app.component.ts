5fad0f65967262c7394520acf34c75c01b8ee19e
Anton Shyrokykh <Anton.Shyrokykh@wolterskluwer.com>
2020-07-29 14:30:25 +0300

    asdasd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
+import { GridSizeChangedEvent, FirstDataRenderedEvent } from 'ag-grid-community';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  title = 'app';
+
+  columnDefs = [
+      {
+        headerName: 'Make', 
+        field: 'make', 
+        minWidth: 550,
+        resizable: true,
+      },
+      {
+        headerName: 'Model', 
+        field: 'model', 
+        minWidth: 150, 
+        resizable: true,
+      },
+      {
+        headerName: 'Price', 
+        field: 'price', 
+        minWidth: 250,
+        resizable: true,
+      }
+  ];
+
+  rowData = [
+      { 
+        make: 'Toyota', 
+        model: 'Celica', 
+        price: 35000
+      },
+      { 
+        make: 'Ford', 
+        model: 'Mondeo', 
+        price: 32000
+      },
+      { 
+        make: 'Porsche', 
+        model: 'Boxter', 
+        price: 72000
+      },
+  ];
+
+  onGridSizeChanged(params: GridSizeChangedEvent): void {
+    params.api.sizeColumnsToFit();
+  }
+
+  onFirstDataRendered(params: FirstDataRenderedEvent): void {
+    params.api.sizeColumnsToFit();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AgGridModule } from 'ag-grid-angular';
+import 'ag-grid-enterprise';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AgGridModule.withComponents([])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
